b23c310ee86a7dbefed37c16e072dc5f8dc40523
Surajattemple <suraj@temple.com>
2026-05-27 13:55:39 +0530

    S

--- new_file_2/file_2.c
+++ new_file_2/file_2.c
@@ -2,6 +2,7 @@
 
 int main() {
     printf("Hello from fw_practice with suraj, here we go again \n");
+    printf("here we go again \n");
     printf("How are you\n");
     printf("file4\n");
     return 0;
--- sensor.c
+++ sensor.c
@@ -2,5 +2,6 @@
 
 int main() {
     printf("Hello from fw_practice with suraj, file4\n");
+    printf("file4\n");
     return 0;
 }
