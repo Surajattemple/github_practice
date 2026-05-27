72915cf55cbdaaebfbe538805a7cd97cab9fb163
Surajattemple <suraj@temple.com>
2026-05-27 13:44:16 +0530

    final_chnages

--- new_file_2/file_2.c
+++ new_file_2/file_2.c
@@ -3,5 +3,6 @@
 int main() {
     printf("Hello from fw_practice with suraj, here we go again \n");
     printf("How are you\n");
+    printf("file4\n");
     return 0;
 }
--- sensor.c
+++ sensor.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello from fw_practice with suraj\n");
+    printf("Hello from fw_practice with suraj, file4\n");
     return 0;
 }
